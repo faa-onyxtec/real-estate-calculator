65518e9552a9685fa93cf0099ce05ef19d25b64d
fatima1234 <fatimahaya.6161@gmail.com>
2023-04-11 01:51:20 +0500

    login/reg

---
+++ app/Notifications/UserResetPassword.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserResetPassword extends Notification
+{
+    use Queueable;
+    public $user, $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $token)
+    {
+        $this->user = $user;
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+        ->subject('Reset Password')
+        ->greeting('Hello ' . $this->user->name . ' !')
+        ->line('You are receiving this email because we
+                received a password reset
+                request for your account.')
+        ->line('This password reset link will expire in 60 minutes.
+                If you did not request a password reset, no further action is
+                required.')
+        ->action('Reset Password', url('api/reset-password/'.$this->token. '/'))
+        ->line('Thank you !');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ForgotPasswordController;
+use App\Http\Controllers\LoginRegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Registeration and verify through email routes
+Route::post('register-post', [LoginRegisterController::class, 'register']);
+Route::get('/user/verify/{token}', [LoginRegisterController::class, 'verifyUser'])->name('user/verify');
+// Login route
+Route::post('post-login', [LoginRegisterController::class, 'login'])->name('peerLogin.post');
+
+// Forget and Reset password route
+Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post');
+Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
+Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
